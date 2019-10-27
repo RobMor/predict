58f4df077d575385b14c533d0e50416050e32ad2
Robert Morrison <robbieguy98@gmail.com>
2019-10-27 15:38:09 +0000

    It works for me
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60155 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/cve.py
+++ predict/cve.py
@@ -1,3 +1,4 @@
+import re
 import time
 
 import json
@@ -7,37 +8,39 @@ from bs4 import BeautifulSoup
 
 
 def get_entry(cve_id):
-    # TODO validate cve_id here!
+    if is_valid_cve(cve_id):
+        entry_in_database = False
 
-    entry_in_database = False
-
-    if entry_in_database:
-        # Retrieve the entry from the database
-        return None
+        if entry_in_database:
+            # Retrieve the entry from the database
+            return None
+        else:
+            return process_entry(scrape_entry(cve_id))
     else:
-        return scrape_entry(cve_id)
+        return None
 
 
-def scrape_entry(cve_id):
-    cve_data = scrape_raw_entry(cve_id)
+def process_entry(entry):
+    links = entry["links"]
 
-    links = cve_data["links"]
+    del entry["links"]
 
-    del cve_data["links"]
-
-    cve_data["github_links"] = []
-    cve_data["normal_links"] = []
+    entry["github_links"] = []
+    entry["normal_links"] = []
 
     for link in links:
         if is_github_link(link):
-            cve_data["github_links"].append(change_github_link(cve_id, link))
+            print(entry)
+            entry["github_links"].append(change_github_link(entry["id"], link))
+        # elif is_git_link(link):
+        #     entry["github_links"].append(change_git_link(cve_id, link))
         else:
-            cve_data["normal_links"].append(link)
+            entry["normal_links"].append(link)
 
-    return cve_data
+    return entry
 
 
-def scrape_raw_entry(cve_id):
+def scrape_entry(cve_id):
     # TODO -- We should be using the NVD
     data = {"id": cve_id}
     url = "https://cve.mitre.org/cgi-bin/cvename.cgi?name={}".format(cve_id)
@@ -63,20 +66,48 @@ def scrape_raw_entry(cve_id):
     return data
 
 
-def is_github_link(link):
-    # TODO more sophisticated verification
+cve_pattern = re.compile("^CVE-\d{4}-\d{4,7}$")
 
-    return "github" in link
+def is_valid_cve(cve_id):
+    return cve_pattern.match(cve_id) is not None
+
+
+github_pattern = re.compile("^(https?:\/\/)?(www\.)?github\.com\/([A-Za-z0-9\-]+)\/([A-Za-z0-9\-]+)\/commit\/([0-9a-f]{5,40})\/?$")
+
+def is_github_link(link):
+    return github_pattern.match(link) is not None
+
+
+# git_pattern = re.compile("^(https?:\/\/)?(www\.)?(git\.[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}).*$") # TODO messy
+
+# def is_git_link(link):
+#     return git_pattern.match(link) is not None
 
 
 def change_github_link(cve_id, link):
-    link = link.split("/")
+    match = github_pattern.match(link)
 
     return link, flask.url_for(
         "info_page",
         cve_id=cve_id,
-        repo_user=link[-4],
-        repo_name=link[-3],
-        hash=link[-1],
+        repo_user=match.group(3),
+        repo_name=match.group(4),
+        hash=match.group(5)
     )
 
+
+# known_github_repositories = {
+#     "git.qemu.org": "github.com/qemu/qemu",
+#     "git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git": "github.com/torvalds/linux",
+# }
+
+# def change_git_link(cve_id, link):
+#     match = git_pattern.match(link)
+
+#     repo_url = match.group(2)
+
+#     if repo_url in known_github_repositories:
+#         repo_user, repo_name = known_github_repositories[repo_url]
+#     else:
+#         # TODO what to do in this case...
+#         raise ValueError("Link is not a known github repository")
