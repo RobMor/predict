25e88fb77245b6766ef789393b33a67900524899
elin1235 <elin1235@943b2781-5fa1-c61d-a95c-b09a2b48979a>
2019-10-28 18:38:13 +0000

    updated setup for pip install sql-alchemy
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60205 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup(
     name="predict",
     packages=["predict"],
-    install_requires=["bs4", "flask", "requests", "flask-login"],
+    install_requires=["bs4", "flask", "requests", "flask-login", "flask-SQLAlchemy"],
     entry_points={
         "console_scripts": ["predict=predict.cli:main"]
     },
