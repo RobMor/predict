e91a0aa27f7ec151d059912d3660062e3797aeab
Robert Morrison <robbieguy98@gmail.com>
2019-10-29 01:28:18 +0000

    Small polishing touches
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60224 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/static/cve_query.js
+++ predict/static/cve_query.js
@@ -1,7 +1,7 @@
 function goto_cve(textbox_id) {
     return function () {
         if (typeof (Storage) !== "undefined") {
-            localStorage.setItem("sidebar", "open")
+            localStorage.setItem("sidebar_status", "open")
         }
 
         window.location.pathname = "cve/" + document.getElementById(textbox_id).value
