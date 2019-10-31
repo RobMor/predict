1114de569f1b3ff6869dc3762f4ccf75da821886
Robert Morrison <robbieguy98@gmail.com>
2019-10-31 02:31:48 +0000

    Added loading icon to search box
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60325 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/static/cve_query.js
+++ predict/static/cve_query.js
@@ -1,10 +1,20 @@
-function goto_cve(textbox_id) {
+function goto_cve(textbox_id, button_id) {
     return function () {
-        if (typeof (Storage) !== "undefined") {
-            localStorage.setItem("sidebar_status", "open")
+        cve = document.getElementById(textbox_id).value
+        
+        // If the user actually gave us a CVE
+        if (cve) {
+            // Change the button to a loading icon!
+            document.getElementById(button_id).innerHTML = "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>"
+            
+            // Set the users local state to have the sidebar open
+            if (typeof (Storage) !== "undefined") {
+                localStorage.setItem("sidebar_status", "open")
+            }
+            
+            // Navigate to the CVE's page
+            window.location.pathname = "cve/" + cve
         }
-
-        window.location.pathname = "cve/" + document.getElementById(textbox_id).value
         // Return false to avoid the real form submission
         return false
     }
