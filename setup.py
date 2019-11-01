cd68cfc5f577c4de1752d29a62f502db92baad9b
Robert Morrison <robbieguy98@gmail.com>
2019-11-01 16:47:57 +0000

    Added skeleton of plugin support
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60354 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- setup.py
+++ setup.py
@@ -7,10 +7,17 @@ from setuptools import setup
 
 setup(
     name="predict",
-    packages=["predict"],
+    packages=["predict", "predict.builtin"],
     install_requires=["bs4", "flask", "requests", "flask-login", "flask-sqlalchemy"],
     entry_points={
-        "console_scripts": ["predict=predict.cli:main"]
+        "console_scripts": ["predict=predict.cli:main"],
+        "predict.plugins": [
+            "csv=predict.builtin.csv:CSV",
+            "majority=predict.builtin.majority:Majority",
+            "current_user=predict.builtin.current_user:CurrentUser",
+            "comments=predict.builtin.comments:Comments",
+            "num_agree=predict.builtin.num_agree:NumAgree",
+        ]
     },
     package_data={
         "predict": ["templates/*", "static/*"]
