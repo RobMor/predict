ddd60c6cc761aebe645b8862804f145a62faf762
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-09 22:47:51 +0000

    created CSV export functionality
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60544 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/builtin/csv.py
+++ predict/builtin/csv.py
@@ -2,7 +2,48 @@ from predict.plugins import FormatPlugin
 import sqlalchemy
 import predict.db
 import predict.models
+from flask import Response, request, Flask
+import flask
 
 class CSV(FormatPlugin):
     id="csv"
     description="CSV"
+
+def createCSV(form):
+	filt = form.get('filters')
+	add = form.get('addData')
+	conRes = form.get('strategy')
+	username = form.get('username')
+	if(filt == "current_user"):
+		labels = (
+			predict.db.Session.query(predict.models.Label)
+			.filter_by(username=username)
+			.all()
+		) or []
+		def generate():
+			for l in labels:
+				yield l.cve_id + "," + l.fix_hash + '\n'
+		return Response(
+			generate(), 
+			mimetype='text/csv',
+			headers={
+						"Content-Disposition":
+						"attachment;filename=labels.csv"
+					}
+		)
+	else:
+		labels = (
+			predict.db.Session.query(predict.models.Label)
+			.all()
+		) or []
+		def generate():
+			for l in labels:
+				yield l.cve_id + "," + l.fix_hash + '\n'
+		return Response(
+			generate(), 
+			mimetype='text/csv', 
+			headers={
+					"Content-Disposition":
+					"attachment;filename=labels.csv"
+				}
+		)
