b22df7230c90bbaa5b4b9deb7bfcf7a4410f8b41
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-12 02:26:23 +0000

    plugins now pull from database as intended.
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60600 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/plugins.py
+++ predict/plugins.py
@@ -1,70 +1,90 @@
 import abc
 import entrypoints
 import flask
+import sqlalchemy
+import predict.db
+import predict.models
 
-
+def LtoList(labels):
+	llist = [[l.cve_id, l.username, l.repo_user, l.repo_name, l.fix_file, l.intro_file, l.fix_hash, l.intro_hash] for l in labels]
+	return llist
+	
 class PluginBase(abc.ABC):
-    @property
-    @abc.abstractproperty
-    def id(self):
-        pass
+	@property
+	@abc.abstractproperty
+	def id(self):
+		pass
 
-    @property
-    @abc.abstractproperty
-    def description(self):
-        pass
+	@property
+	@abc.abstractproperty
+	def description(self):
+		pass
 
 
 # TODO
 class FilterPlugin(PluginBase, abc.ABC):
-    pass
+	@abc.abstractmethod
+	def __init__(self, query):
+		"""Defines the construction of this filter.
+
+		Args:
+			query: query object being used for export.
+		"""
+		pass
+	
+	@abc.abstractmethod
+	def filterq(self):
+		"""Defines the functionality associated with this filter.
+		Handles construction of a flask response to be handed back to he user.
+		"""
+		pass
 
 
 # TODO
 class DataPlugin(PluginBase, abc.ABC):
-    pass
+	pass
 
 
 # TODO
 class ConflictPlugin(PluginBase, abc.ABC):
-    pass
+	pass
 
 
 # TODO
 class FormatPlugin(PluginBase, abc.ABC):
-    @abc.abstractmethod
-    def __init__(self, data):
-        """Defines the construction of this export format.
+	@abc.abstractmethod
+	def __init__(self, data):
+		"""Defines the construction of this export format.
 
-        Args:
-            data (list of lists): The data to be exported. Each sub-list is a datapoint
-        """
-        pass
+		Args:
+			data (list of lists): The data to be exported. Each sub-list is a datapoint
+		"""
+		pass
 
-    @abc.abstractmethod
-    def generate(self):
-        """Defines the functionality associated with this file format.
+	@abc.abstractmethod
+	def generate(self):
+		"""Defines the functionality associated with this file format.
 
-        Handles construction of a flask response to be handed back tto he user.
-        """
-        pass
+		Handles construction of a flask response to be handed back to he user.
+		"""
+		pass
 
 
 def load_plugins():
-    plugins = {}
-    for name, entrypoint in entrypoints.get_group_named("predict.plugins").items():
-        source = entrypoint.load()
+	plugins = {}
+	for name, entrypoint in entrypoints.get_group_named("predict.plugins").items():
+		source = entrypoint.load()
 
-        if issubclass(source, FilterPlugin):
-            plugins["filter"] = plugins.get("filter", []) + [source]
-        elif issubclass(source, DataPlugin):
-            plugins["data"] = plugins.get("data", []) + [source]
-        elif issubclass(source, ConflictPlugin):
-            plugins["conflict"] = plugins.get("conflict", []) + [source]
-        elif issubclass(source, FormatPlugin):
-            plugins["format"] = plugins.get("format", []) + [source]
+		if issubclass(source, FilterPlugin):
+			plugins["filter"] = plugins.get("filter", []) + [source]
+		elif issubclass(source, DataPlugin):
+			plugins["data"] = plugins.get("data", []) + [source]
+		elif issubclass(source, ConflictPlugin):
+			plugins["conflict"] = plugins.get("conflict", []) + [source]
+		elif issubclass(source, FormatPlugin):
+			plugins["format"] = plugins.get("format", []) + [source]
 
-    return plugins
+	return plugins
 
 
 # Data Export Process
@@ -74,9 +94,13 @@ def load_plugins():
 # 4. Select Output Format (required)
 
 def export(filter_, extra_data, strategy, file_format):
-    data = [["testing", "12"], ["34", "56"]]
+	q = predict.db.Session.query(predict.models.Label)
+	
+	if(filter_ != "none"):
+		filter = entrypoints.get_single("predict.plugins", filter_).load()
+		filter = filterq(something, q)
+		q = filter.filter()
+	file_format = entrypoints.get_single("predict.plugins", file_format).load()
+	file_format = file_format(LtoList(q.all()) or [])
 
-    file_format = entrypoints.get_single("predict.plugins", file_format).load()
-    file_format = file_format(data)
-
-    return file_format.generate()
+	return file_format.generate()
