1b722083351318a3d08e875c2b37a4fc33c2c924
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-12 19:46:52 +0000

    Adding additional information in form of comments now supported.
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60626 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/builtin/comments.py
+++ predict/builtin/comments.py
@@ -2,5 +2,18 @@ from predict.plugins import DataPlugin
 
 
 class Comments(DataPlugin):
-    id="comments"
-    description="Comments"
+	id="comments"
+	description="Comments"
+	
+	def __init__(self, llist, q):
+		self.llist = llist
+		self.query = q
+		
+	def add_data(self):
+		nlist = self.llist
+		clist = [l.comment for l in self.query]
+		nlist[0] = nlist[0] + ["Comment"]
+		for i in range(1, len(nlist)):
+			nlist[i] = nlist[i] + [clist[i - 1]]
+			
+		return nlist
