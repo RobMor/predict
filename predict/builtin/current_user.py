52bbb4be2a54d14949e197af5fa1c00e1aa14357
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-12 02:44:30 +0000

    Filtering functionality for this plug-in is now implemented.
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60609 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/builtin/current_user.py
+++ predict/builtin/current_user.py
@@ -1,6 +1,14 @@
 from predict.plugins import FilterPlugin
-
+import flask_login
+import socket
 
 class CurrentUser(FilterPlugin):
-    id="current_user"
-    description="Only Me"
+	id="current_user"
+	description="Only Me"
+
+	def __init__(self, query):
+			self.username = flask_login.current_user.get_id() or socket.gethostname()
+			self.query = query
+
+	def filterq(self):
+		return self.query.filter_by(username=self.username)
