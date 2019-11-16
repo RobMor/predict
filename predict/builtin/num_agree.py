f028a5722aa9b907ab4aa9556de9ef732facf28a
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-16 02:32:30 +0000

    Now correctly lists number of agreements.
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60845 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/builtin/num_agree.py
+++ predict/builtin/num_agree.py
@@ -2,5 +2,25 @@ from predict.plugins import DataPlugin
 
 
 class NumAgree(DataPlugin):
-    id="num_agree"
-    description="Number In Agreement"
+	id="num_agree"
+	description="Number In Agreement"
+
+	
+	def __init__(self, llist, q):
+		self.llist = llist
+		self.query = q
+		
+	def add_data(self):
+		nlist = self.llist
+		nlist[0] = nlist[0] + ["Number in Agreement"]
+		for i in range(1, len(nlist)):
+			count = 0
+			for j in range(1, len(nlist)):
+				if i != j and nlist[i][0] == nlist[j][0] \
+						and nlist[i][4] == nlist[j][4] and nlist[i][5] == nlist[j][5] and \
+						nlist[i][6] == nlist[j][6] and nlist[i][7] == nlist[j][7]:
+					count += 1
+			nlist[i] = nlist[i] + [count]
+							
+			
+		return nlist
