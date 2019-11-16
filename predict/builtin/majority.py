ed10e624e34b1d3d056f8085b5bcb2b19c1a3cd4
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-16 02:32:08 +0000

    Not currently functional. Commmiting changes for safety purposes. Shouldn't be used yet, but probably won't break.
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60844 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/builtin/majority.py
+++ predict/builtin/majority.py
@@ -2,5 +2,28 @@ from predict.plugins import ConflictPlugin
 
 
 class Majority(ConflictPlugin):
-    id="majority"
-    description="Majority Rule"
+	id="majority"
+	description="Majority Rule"
+	
+	def __init__(self, l, query):
+		self.l = l
+		self.query = query
+
+	def resolve(self):
+		nl = []
+		l = self.l
+		templ = []
+		already = False
+		"""
+		for i in range(1, len(l)):
+			for k in range(1, len(nl)):
+				if l[i][0] == nl[k][0]:
+					already = True
+			if not already:
+				for k in range(1, len(l)):
+					if l[i][0] == l[k][0] and l[i][2] == l[k][2] and l[i][3] == l[k][3]:
+						if l[i][4] == l[k][4] and l[i][5] == l[k][5] and l[i][6] == l[k][6] and l[i][7] == l[k][7]:
+							
+			already = False	
+			"""
+		return nl
