325cb1606152b79207af224688fd80c2d3587b84
Robert Morrison <robbieguy98@gmail.com>
2019-11-17 03:42:13 +0000

    Made the javascript not re-request icons it has already seen...
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@60898 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/static/js/icons.js
+++ predict/static/js/icons.js
@@ -1,6 +1,8 @@
 /*
  * Replace all SVG images with inline SVG
  */
+var alreadySeen = {}
+
 $(function(){
     $('img.icon').each(function (index, element) {
         replaceImgWithSVG(element)
@@ -13,30 +15,39 @@ function replaceImgWithSVG(img) {
     var imgClass = $img.attr('class');
     var imgURL = $img.attr('src');
 
+    if (imgURL in alreadySeen) {
+        replaceImgWithSVGData(alreadySeen[imgURL])
+        return
+    }
+
     jQuery.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
-
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
-
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
-        
-        // Check if the viewport is set, else we gonna set it if we can.
-        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-        }
-
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-
+        alreadySeen[imgURL] = data
+        replaceImgWithSVGData(data)
     }, 'xml');
+}
+
+function replaceImgWithSVGData(data) {
+    // Get the SVG tag, ignore the rest
+    var $svg = jQuery(data).find('svg');
+
+    // Add replaced image's ID to the new SVG
+    if(typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+    }
+    // Add replaced image's classes to the new SVG
+    if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+
+    // Remove any invalid XML tags as per http://validator.w3.org
+    $svg = $svg.removeAttr('xmlns:a');
+    
+    // Check if the viewport is set, else we gonna set it if we can.
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+
+    // Replace image with new SVG
+    $img.replaceWith($svg);
 
 }
