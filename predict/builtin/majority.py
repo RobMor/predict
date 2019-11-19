fbf6c926748b165a3bab25eddae2241abf90bb9e
ThomasGiddings <thomas.giddings.e@gmail.com>
2019-11-19 21:14:39 +0000

    Implemented majority rule plug-in functionality.
    git-svn-id: https://vis.cs.umd.edu/svn/projects/predict/prod@61096 943b2781-5fa1-c61d-a95c-b09a2b48979a

--- predict/builtin/majority.py
+++ predict/builtin/majority.py
@@ -13,17 +13,48 @@ class Majority(ConflictPlugin):
 		nl = []
 		l = self.l
 		templ = []
-		already = False
-		"""
-		for i in range(1, len(l)):
-			for k in range(1, len(nl)):
-				if l[i][0] == nl[k][0]:
+		initi = False
+		for i in l[0]:
+			if("Number in Agreement" == i):
+				initi = True
+		if not initi:
+			l[0] = l[0] + ["Number in Agreement"]
+			for i in range(1, len(l)):
+				count = 0
+				for j in range(1, len(l)):
+					if i != j and l[i][0] == l[j][0] \
+							and l[i][4] == l[j][4] and l[i][5] == l[j][5] and \
+							l[i][6] == l[j][6] and l[i][7] == l[j][7]:
+						count += 1
+				l[i] = l[i] + [count]
+			
+		loc = 0
+		for i in range(0, len(l[0])):
+			if l[0][i] == "Number in Agreement":
+				loc = i
+		nl += [l[0]]
+		for item in range(1, len(l)):
+			already = False
+			for i in nl:
+				if l[item][0] == i[0]:
 					already = True
 			if not already:
-				for k in range(1, len(l)):
-					if l[i][0] == l[k][0] and l[i][2] == l[k][2] and l[i][3] == l[k][3]:
-						if l[i][4] == l[k][4] and l[i][5] == l[k][5] and l[i][6] == l[k][6] and l[i][7] == l[k][7]:
-							
-			already = False	
-			"""
+				highest = l[item][loc]
+				hi = l[item]
+				for i in range(1, len(l)):
+					if (l[i][loc] > highest):
+						highest = l[i][loc]
+						hi = l[i]
+				nl += [hi]
+		
+		if not initi:
+			temp = []
+			for i in range(0, len(nl)):
+				temp2 = []
+				for j in range(0, len(nl[i])):		
+					if j != loc:
+						temp2 += [nl[i][j]]
+				temp += [temp2]
+			nl = temp
+		
 		return nl
